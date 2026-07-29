2c72a52b063eacbb152c45cbb76fb01e606cc734
Aman-kumar <98403770+Aman-kumar-2007@users.noreply.github.com>
2026-07-29 19:06:27 +0530

    Firebase authentication and Firestore imports

---
+++ js/firebase/auth.js
@@ -0,0 +1,15 @@
+import { auth, db } from "../firebase/config.js";
+import { getFirebaseErrorMessage } from "../utils/firebaseErrors.js";
+
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+    doc,
+    setDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
