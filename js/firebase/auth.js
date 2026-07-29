69732b39e9e943b35f1a2ee5e9c7ed535ed8bab2
Aman-kumar <98403770+Aman-kumar-2007@users.noreply.github.com>
2026-07-29 19:06:47 +0530

    Implement user authentication functions

--- js/firebase/auth.js
+++ js/firebase/auth.js
@@ -13,3 +13,99 @@ import {
     setDoc,
     serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
+export async function registerUser(name, email, password) {
+    try {
+
+        const userCredential = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+        const user = userCredential.user;
+
+
+        await setDoc(doc(db, "users", user.uid), {
+            uid: user.uid,
+            name,
+            email,
+            photoURL: "https://i.pinimg.com/236x/13/74/20/137420f5b9c39bc911e472f5d20f053e.jpg",
+            createdAt: serverTimestamp()
+        });
+
+        return {
+            success: true,
+            user
+        };
+
+    } catch (error) {
+        return {
+            success: false,
+            error: getFirebaseErrorMessage(error.code)
+        };
+
+    }
+}
+
+
+export async function loginUser(email, password) {
+
+    try {
+
+        const userCredential = await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        return {
+            success: true,
+            user: userCredential.user
+        };
+
+    } catch (error) {
+
+        return {
+            success: false,
+            error: getFirebaseErrorMessage(error.code)
+        };
+
+    }
+
+}
+
+export async function logoutUser() {
+    try {
+
+        sessionStorage.clear();
+
+        localStorage.removeItem("draftEmail");
+        localStorage.removeItem("selectedMail");
+        localStorage.removeItem("theme");
+        localStorage.removeItem("filters");
+
+        await signOut(auth);
+
+        return {
+            success: true
+        };
+
+    } catch (error) {
+
+        return {
+            success: false,
+            error: getFirebaseErrorMessage(error.code)
+        };
+    }
+}
+
+export function checkAuth(callback) {
+
+    onAuthStateChanged(auth, (user) => {
+
+        callback(user);
+
+    });
+
+}
